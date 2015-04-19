1d4cec151831d11b9f003f890e3a777ba601835a
elabbiglubu <james.da.4th@gmail.com>
2015-04-19 13:25:44 -0400

    testing different input numbers

--- aruco/aru_3_board_stuff/just_run.sh
+++ aruco/aru_3_board_stuff/just_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./build/based_on_aruco_test live ./build/td/22board2config.yml ./build/td/22board3config.yml ./build/td/intrinsics.yml 0.065 0 0 1 0 0 1
+./build/based_on_aruco_test live ./build/td/22board2config.yml ./build/td/22board3config.yml ./build/td/intrinsics.yml 0.065 0 -1 -0.5 0 0 1
